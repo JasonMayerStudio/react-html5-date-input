96918b03e81ba1ab730236292ecea6f1588a3861
Wen, Yadong (Ralph) <yawen@microstrategy.com>
2016-12-28 22:45:01 -0500

    update handlers for hour

--- src/Html5TimeInput.js
+++ src/Html5TimeInput.js
@@ -3,13 +3,18 @@ import classNames from 'classnames';
 
 import './Html5TimeInput.css';
 
+const MINUTE = 60;
+const HOUR = 60 * MINUTE;
+const ALL_DAY = 24 * HOUR;
+const HALF_DAY = ALL_DAY / 2;
+
 class TimeInpt extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
       editIndex: -1,
-      ampm: 'AM'
+      sec: 0
     };
 
     this.onHourClick = this.onHourClick.bind(this);
@@ -76,24 +81,47 @@ class TimeInpt extends Component {
 
   handleKey(key) {
     switch(this.state.editIndex) {
+    case 0:
+      this.handleKeyHour(key);
+      break;
     case 3:
       this.handleKeyAmpm(key);
       break;
     }
   }
 
+  handleKeyHour(key) {
+    let newSec = this.state.sec;
+    switch(key) {
+    case 'ArrowUp':
+      newSec += HOUR;
+      break;
+    case 'ArrowDown':
+      newSec -= HOUR;
+      break;
+    }
+
+    newSec += ALL_DAY;
+    newSec %= ALL_DAY;
+
+    this.setState({
+      sec: newSec
+    });
+  }
+
   handleKeyAmpm(key) {
-    let newAmpm = this.state.ampm;
+    let newSec = this.state.sec;
     if (key === 'ArrowUp' || key === 'ArrowDown') {
-      newAmpm = this.state.ampm === 'AM' ? 'PM' : 'AM';
+      newSec = (this.state.sec + HALF_DAY) % ALL_DAY;
     } else if (key === 'a' || key === 'A') {
-      newAmpm = 'AM';
+      newSec %= HALF_DAY;
     } else if (key === 'p' || key === 'P') {
-      newAmpm = 'PM';
+      newSec %= HALF_DAY;
+      newSec += HALF_DAY;
     }
 
     this.setState({
-      ampm: newAmpm
+      sec: newSec
     });
   }
 
@@ -114,15 +142,26 @@ class TimeInpt extends Component {
       'TimeInpt--selected': this.state.editIndex === 3
     });
 
+    let hour = this.state.sec / HOUR;
+    if (hour > 12) {
+      hour -= 12;
+    }
+    hour = hour.toString();
+    if (hour.length < 2) {
+      hour = '0' + hour;
+    }
+
+    let ampm = this.state.sec < HALF_DAY ? 'AM' : 'PM';
+
     return (
       <span className="TimeInpt" contentEditable={true} suppressContentEditableWarning onKeyDown={this.onKeyDown} onChange={() => {}}>
         <span contentEditable={false}>
-          <span className={hourCls} onClick={this.onHourClick}>12</span>
+          <span className={hourCls} onClick={this.onHourClick}>{hour}</span>
           <span className="TimeInpt-colon">:</span>
           <span className={minuteCls} onClick={this.onMinuteClick}>00</span>
           <span className="TimeInpt-colon">:</span>
           <span className={secondCls} onClick={this.onSecondClick}>00</span>
-          <span className={ampmCls} onClick={this.onAmpmClick}>{this.state.ampm}</span>
+          <span className={ampmCls} onClick={this.onAmpmClick}>{ampm}</span>
         </span>
       </span>
     );
