730558e776689b74ded12b2a0dd753cb19230f7f
Wen, Yadong (Ralph) <yawen@microstrategy.com>
2017-01-03 12:17:22 -0500

    add gif for demo

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-# Html5TimeInput
+# react-html5-time-input
 
 A cross browser HTML5 time input.
+
+![screen cast](https://raw.githubusercontent.com/ralphite/react-html5-time-input/master/react-html5-time-input-gif.gif)
--- src/index.js
+++ src/index.js
@@ -6,10 +6,7 @@ import './index.css';
 
 ReactDOM.render(
   <div className="App">
-    once update a time &nbsp;
     <TimeInput sec={15*3600+35*60+8} onChangeCallback={(time) => { console.log(time); }}/>
-    &nbsp; wants to compare with b time &nbsp;
-    <input type="time" />
   </div>,
   document.getElementById('root')
 );
